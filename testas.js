e7e76f82f8837a06b9831a4756a76d5d153cb862
Dovilė <dovile.keparute@gmail.com>
2021-10-06 14:21:07 +0300

    Naujas testas

--- testas.js
+++ testas.js
@@ -31,9 +31,30 @@ console.log(obj1.labs);
 
 class Grybas {
     constructor(tipas) {
-        this.tipas = "Baravykas";
-        this.tipas = "Ūmedė";
-        this.tipas = "Lepšis";
+        this.tipas = tipas;
     }
 }
-console.log(Grybas);
+let g1 = new Grybas("Baravykas");
+let g2 = new Grybas("Ūmedė");
+let g3 = new Grybas("Lepšis");
+
+// //4. Duotas masyvas, masyvą iteruoti ciklu forEach ir console.log kiekvienoje
+// // iteracijoje atspausdinti indeksą ir jo reikšmę, jeigu ta reikšmė prasideda raide A arba a
+
+// const mas4 = ['Agurkas', 'Bananas', 'arbūzas', 'Bebras', 'Voverė', 'Ąžuolas', 'Mažas triušis']
+
+
+
+// //5. Duotas stringas, suskaičiuoti kiek stringe yra raidžių z
+// const str1 = 'Storas zebras, bėga nuo zuulu genties nedžiotojų.';
+
+// //6. Duotas Setas, į kurį reikia pridėti reikšmes (stringus) 'Labas' ir 'Ate' ir console.log
+// // atspausdinti Seto ilgį
+// const set1 = new Set(['Labas', 'Vakaras', 'Rytas', 'Valio']);
+
+
+ 
+// //7. Suprototaipinti standartinį String tipo objektą, pridedant metodą lastCharIs(), 
+// // kuris grąžintų paskutinį stringo simbolį. Pademonstruoti veikimą su stringu 'Ku Ku';
+// const str2 = 'Ku ku';
+// // console.log(str2.lastCharIs()) po prototaipinimo eilutė turi veikti
